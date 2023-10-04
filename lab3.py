e4815d3182363567cb33a45f8b038e19e52c26a6
ershooooo <ershovaclara@yandex.ru>
2023-10-05 00:09:46 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -20,6 +20,42 @@ def form():
         errorss['age'] = 'Заполните поле!'
 
     sex = request.args.get('sex')
-    
+
     return render_template('form1.html', user=user, age=age, sex=sex,errors=errors,errorss=errorss)  
     
+
+@lab3.route('/lab3/order')
+def order():
+    drink=request.args.get('drink')
+    milk=request.args.get('milk')
+    sugar=request.args.get('sugar')
+    return render_template('order.html', drink=drink, milk=milk, sugar=sugar)
+
+@lab3.route('/lab3/pay')
+def pay():
+    price=0
+    drink=request.args.get('drink')
+    #Пусть кофе стоит 120 рублей, чёрный чай — 80 рублей, зелёный — 70 рублей. 
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    #Добавка молока удорожает напиток на 30 рублей, а сахара — на 10. Тогда получим:
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    card=request.args.get('card')
+    name=request.args.get('name')
+    cvv=request.args.get('cvv')
+
+    return render_template('pay.html',price=price,card=card,name=name,cvv=cvv)
+
+
+@lab3.route('/lab3/succes')
+def succes():
+    return render_template('succes.html')

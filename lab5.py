f765a3c93389efe9a0b01d371c9a9ee9b78e5a69
Clara <ershovaclara@yandex.ru>
2023-11-08 23:43:25 +0700

    Пробная

--- lab5.py
+++ lab5.py
@@ -4,48 +4,52 @@ import psycopg2
 lab5 = Blueprint('lab5',__name__)
 
 
-@lab5.route('/lab5')
-def main():
-#Прописываем параметры подключения к БД
+def dbConnect():
+    # Прописываем параметры подключения к БД
     conn = psycopg2.connect(
         host="127.0.0.1",
         database="knowledge_base_for_ersh_trub",
         user="ersh_trub_knowledge_base",
         password="ershtrub123")
+    return conn
 
-#Получаем курсор. С помощью него мы можем выполнять sql-запросы
+
+def dbClose(cursor,connection):
+    # Закрываем курсор и соединение 
+    cursor.close()
+    connection.close()
+
+
+@lab5.route('/lab5')
+def main():
+
+    conn = dbConnect()
+    # Получаем курсор. С помощью него мы можем выполнять sql-запросы
     cur=conn.cursor()
 
-#Пишем запрос, который psycopg2 должен выполнить
+    # Пишем запрос, который psycopg2 должен выполнить
     cur.execute("SELECT * FROM users;")
-# fetchall - получить все строки, которые получились в результате выполнения sql -запроса в execute
-# Сохраняем эти строки в перемннную RESULT
+    # fetchall - получить все строки, которые получились в результате выполнения sql -запроса в execute
+    # Сохраняем эти строки в перемннную RESULT
     result=cur.fetchall()
-
-#Закрываем соеденения с БД
-    cur.close()
-    conn.close()
-
     print(result)
+    dbClose(cur,conn)
     return "go to console"
 
 @lab5.route('/lab5/users')
 def get_users():
 
-    conn = psycopg2.connect(
-        host="127.0.0.1",
-        database="knowledge_base_for_ersh_trub",
-        user="ersh_trub_knowledge_base",
-        password="ershtrub123")
+    conn = dbConnect()
     cur = conn.cursor()
     
     cur.execute("SELECT username FROM users;")
     result = cur.fetchall()
 
-    cur.close()
-    conn.close()
+    dbClose(cur,conn)
 
     username = ""
     for row in result:
         username += f"{row[0]}\n"
     return username
+
+

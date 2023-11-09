926cf939a4dff6affaf8b9356a70e73e2d7ae227
Clara <ershovaclara@yandex.ru>
2023-11-09 19:58:22 +0700

    Страница регистрации

--- lab5.py
+++ lab5.py
@@ -43,3 +43,49 @@ def get_users():
     return username
 
 
+@lab5.route('/lab5/register', methods=['GET','POST'])
+def registerPage():
+    errors=[]
+
+    # Если это метод GET, то верни шаблон и заверши выполнение
+    if request.method=='GET':
+        return render_template('register.html',errors=errors)
+    
+    # Если мы попали сюда, значит это метод POST, так как GET мы уже обработали и сделали return.
+    # После return функция немедленно завершается
+    username=request.form.get('username')
+    password=request.form.get('password')
+
+    # Проверяем username и password на пустоту
+    # Если любой из них пустой, то добавляем ошибку и рендерим шаблон
+    if username =='' or password == '':
+        errors='Пожалуйста, заполните все поля'
+        return render_template('register.html',errors=errors)
+
+    # Если мы попали сюда, значит username и password заполнены
+    # Подключаемся к БД
+    conn = dbConnect()
+    cur = conn.cursor()
+
+    # Проверяем наличие клиента в базе. У нас не может быть два пользователя с одинаковыми логинами
+    # WARNING: мы используем f-строки, что не рекомендуется делать 
+    cur.execute(f"SELECT username FROM users WHERE username = '{username}';")
+
+    # fetchone, в отличие от fetchall, получает только одну строку
+    # мы задали свойство UNIQUE для пользователя, значитбольше одной строки мы не можем получить 
+    # Только один пользователь с таким именем может быть в БД
+    if cur.fetchone() is not None:
+        errors='Пользователь с данным именем уже существует'
+        
+        dbClose(cur,conn)
+        return render_template('register.html',errors=errors)
+
+    # Если мы попали сюда, то значит в cur.fetchone нет ни одной строки
+    # Значит пользователя с таким же логином не существует
+    cur.execute(f"INSERT INTO users (username,password) VALUES ('{username}','{password}');")
+
+    # Делаем commit - фиксируем изменения
+    conn.commit()
+    dbClose(cur,conn)
+
+    return redirect('/lab5/login')

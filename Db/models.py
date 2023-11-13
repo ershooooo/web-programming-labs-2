ebfba434680ecd8647471c8a9143e7542ff26e3c
Clara <ershovaclara@yandex.ru>
2023-11-14 01:06:19 +0700

    5_1

---
+++ Db/models.py
@@ -0,0 +1,20 @@
+#Импортируем переменную db из файла __init.py__
+from . import db
+from flask_login import UserMixin
+
+#Описываем схему нашей БД в виде оюъектов таким образом, создание таблиц (схемы БД)
+#Возьмет на себя SQLAlchemy - система ORM.abs
+
+class users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(102), nullable=False)
+
+class articles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(50), nullable=False)
+    article_text = db.Column(db.Text, nullable=False)
+    is_favorite = db.Column(db.Boolean)
+    is_public = db.Column(db.Boolean)
+    likes = db.Column(db.Integer)
---
+++ lab6.py
@@ -0,0 +1,6 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Blueprint, render_template, request, make_response, redirect, session
+import psycopg2
+
+lab6 = Blueprint('lab6',__name__)
+

996308e1e126813a75d5713cfe46bf11c147deda
Clara <ershovaclara@yandex.ru>
2023-11-21 03:06:18 +0700

    Доп

--- lab6.py
+++ lab6.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, request, make_response, redirect, 
 import psycopg2
 from Db import db
 from Db.models import users,articles
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 
 lab6 = Blueprint('lab6',__name__)
 
@@ -98,5 +98,56 @@ def login():
         if check_password_hash(my_user.password, password_form):
             #Сохраняем JWT токен
             login_user(my_user,remember=False)
-            return redirect('/lab6/articles')
-    return render_template('login.html')
+            return redirect('/lab6')
+    return render_template('login.html',username=current_user.username)
+
+@lab6.route("/lab6/articles")
+@login_required
+def articles_list():
+    my_articles = articles.query.filter_by(user_id=current_user.id).all()
+    return render_template('spisok_article.html',articles=my_articles)
+
+@lab6.route("/lab6/articles/<int:article_id>")
+@login_required
+def get_article(article_id):
+    article = articles.query.filter_by(id=article_id, user_id=current_user.id).first()
+    if article is None:
+        return "Not found!"
+    text = article.article_text.splitlines()
+    return render_template("articleN.html", article_text=text, article_title=article.title, username=current_user.username)
+
+@lab6.route('/lab6/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/lab6')
+
+#Добавление статей
+@lab6.route("/lab6/new_article", methods=['GET','POST'])
+@login_required
+def createArticle():
+    errors = ''
+    if request.method == "GET":
+        return render_template("new_article.html")
+    if request.method == "POST":
+        text_article = request.form.get("text_article")
+        title = request.form.get("title_article")
+        if len(text_article) == 0:
+            errors = 'Заполните текст'
+            return render_template("new_article.html", errors=errors)
+        new_article = articles(user_id=current_user.id, title=title, article_text=text_article)
+        db.session.add(new_article)
+        db.session.commit()
+        
+        return redirect(f"/lab6/articles/{new_article.id}")
+
+
+@lab6.route('/lab6')
+def all():
+    if current_user.is_authenticated:
+        # если пользователь авторизирован
+        visibleUser = current_user.username
+    else:
+        # если пользователь не авторизирован
+        visibleUser = 'Anon'
+    return render_template('6_main.html', username=visibleUser)

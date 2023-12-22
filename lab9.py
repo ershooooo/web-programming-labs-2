2692e68e250387835c0232a6f89568c34df703bc
Clara <ershovaclara@yandex.ru>
2023-12-22 21:53:31 +0700

    Обработчик ошибки 404

--- lab9.py
+++ lab9.py
@@ -5,3 +5,12 @@ lab9=Blueprint('lab9',__name__)
 @lab9.route('/lab9/')
 def main():
     return render_template('lab9/index.html')
+
+@lab9.app_errorhandler(404)
+def not_found(err):
+    return render_template('lab9/404.html'), 404
+
+
+
+
+

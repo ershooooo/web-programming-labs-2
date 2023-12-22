34d9d6ce45cb4e923ee91b3c44e71efa9bf48741
Алиса Трубицына <trubitsyna_al@mail.ru>
2023-12-23 01:56:31 +0700

    Последнии правки

--- lab8.py
+++ lab8.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request, abort
-
+from flask import Blueprint, render_template, request, abort, jsonify
+import datetime
 lab8=Blueprint('lab8',__name__)
 
 @lab8.route('/lab8/')
@@ -14,36 +14,47 @@ courses = [
 
 #Весь список курсов
 @lab8.route('/lab8/api/courses/', methods=['GET'])
-def  get_courses():
-    return courses
+def get_courses2():
+    return jsonify (courses)
 
 #Конкретный курс по номеру
 @lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
-def get_course(course_num):
-    if course_num < 0 or course_num > len(courses)-1:
+def get_courses3(course_num):
+    if 0<= course_num < len(courses)-1:
+        return courses[course_num]
+    else:
         abort(404, "Course not found")
-    return courses[course_num]
+    
 
 #Удаление курса
 @lab8.route('/lab8/api/courses/<int:course_num>',methods=['DELETE'])
-def del_course(course_num):
-    if course_num < 0 or course_num > len(courses)-1:
+def del_courses(course_num):
+    if 0<= course_num < len(courses)-1:
+        del courses[course_num]
+        return '', 204
+    else:
         abort(404, "Course not found")
-    del courses[course_num]
-    return '', 204
+    
+    
 
 #Редактирование существующего курса
 @lab8.route('/lab8/api/courses/<int:course_num>',methods=['PUT'])
-def put_course(course_num):
-    if course_num < 0 or course_num > len(courses)-1:
-        abort(404, "Course not found")
-    course = request.get_json()
+def put_courses(course_num):
     courses[course_num] = course
-    return courses[course_num]
+    course = request.get_json()
+
+    if 0<=  course_num < len(courses)-1:
+        return courses[course_num]
+    else:
+        abort(404, "Course not found")
+    
+    
+    
 
 #Добавление нового курса
 @lab8.route('/lab8/api/courses/', methods=['POST'])
 def add_course():
     course = request.get_json()
     courses.append(course)
+    course["date_"] = datetime.datetime.now().strftime('%Y-%m-%d')
     return {"num": len(courses)-1}

6948923a4203ffbc0e786645c74522c83f08a1c7
Clara <ershovaclara@yandex.ru>
2023-12-22 23:03:54 +0700

    Открытка

--- lab9.py
+++ lab9.py
@@ -1,10 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 lab9=Blueprint('lab9',__name__)
 
-@lab9.route('/lab9/')
+@lab9.route('/lab9/', methods=['GET', 'POST'])
 def main():
-    return render_template('lab9/index.html')
+    if request.method =='GET':
+        return render_template('lab9/index.html')
+    if request.method == 'POST':
+        user_1 = request.form.get('user_1')
+        gender = request.form.get('gender')
+        user_2 = request.form.get('user_2')
+        return render_template('lab9/postcard.html', gender=gender, user_1=user_1, user_2=user_2)
 
 @lab9.app_errorhandler(404)
 def not_found(err):
